bdd3488131f6f54bc540da11e645bfcc05afba44
Tarnished <susan.dev291@gmail.com>
2026-07-16 15:44:11 +0545

    concurency rate limit #1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use syncify::test::integration::wire;
 
 // Single Track
-const URL: &str = "https://open.spotify.com/track/3zEN0ii6s4DHHBpnTp3RP7?si=39268f955cd84950";
+const TRACK: &str = "https://open.spotify.com/track/3zEN0ii6s4DHHBpnTp3RP7?si=39268f955cd84950";
 
 // Playlist
-// const URL: &str = "https://open.spotify.com/playlist/6XIAWqIZ2oGU3b2g6nX2O0?si=6f8028425afb44bb";
+const URL: &str = "https://open.spotify.com/playlist/2DIftVgibXX2uaTEf6L4a3?si=e6a1d51749ef47d5";
+
+const TEST: &str = "https://open.spotify.com/playlist/6XIAWqIZ2oGU3b2g6nX2O0?si=84d0a92aeb424e0c";
 
 #[tokio::main]
 async fn main() {
--- src/test/integration.rs
+++ src/test/integration.rs
@@ -1,24 +1,37 @@
-use std::time::{Instant};
-
 use crate::spotify::{playlist::get_playlist_metadata, track::get_song_details};
-
-
+use futures::stream::{self, StreamExt};
+use macro_colors::Colorize;
+use macro_colors::green_println;
+use std::time::Instant;
 
 pub async fn wire(url: &str) {
-    let start =  Instant::now();
+    let start = Instant::now();
 
     if url.contains("/track/") {
-    let song = get_song_details(url).await.unwrap();
-    println!("Title: {:?}", song.title);
-    println!("Artist: {:?}", song.description.artist);
-    println!("Album: {:?}", song.description.album);
-    println!("Year: {:?}", song.description.year);
-    println!("Duartion: {:?}", song.duration);
+        process_song(url).await;
     } else {
-        let song = get_playlist_metadata(url).await.unwrap();
+        let songs = get_playlist_metadata(url).await.unwrap();
+
+
+        stream::iter(&songs)
+            .for_each_concurrent(3, |track| async move {
+                process_song(&track).await;
+            })
+            .await;
+        println!("{} Songs Found Inside the playlist", &songs.len());
     }
 
-    println!("======================");
     println!("Time Taken {:?}", start.elapsed());
     println!("======================");
+}
+
+async fn process_song(song: &str) {
+    let song = get_song_details(song).await.unwrap();
+    println!("Data From Spotify");
+    green_println!("Title: {:?}", song.title);
+    green_println!("Artist: {:?}", song.description.artist);
+    green_println!("Album: {:?}", song.description.album);
+    green_println!("Year: {:?}", song.description.year);
+    green_println!("Duartion: {:?}", song.duration);
+    green_println!("======================");
 }
